26a76354c2bf7ddf4b55dba421af6240f88dc348
Mohammad Eisa <68770306+EISAGHANI@users.noreply.github.com>
2023-04-10 21:24:09 +0200

    init

---
+++ new.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("sod-form");
+const totalSqftOutput = document.getElementById("total-sqft");
+// const rollsNeededOutput = document.getElementById("rolls-needed");
+const resultsContainer = document.getElementById("results");
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+	// Get input values
+	const length = parseFloat(document.getElementById("length").value);
+	const width = parseFloat(document.getElementById("width").value);
+	// const sqftPerRoll = parseFloat(document.getElementById("sqft-per-roll").value);
+
+	// Calculate total square footage and rolls needed
+	const totalSqft = length * width;
+	// const rollsNeeded = Math.ceil(totalSqft / sqftPerRoll);
+
+	// Output results
+	totalSqftOutput.textContent = totalSqft;
+	// rollsNeededOutput.textContent = rollsNeeded;
+	resultsContainer.style.display = "block";
+});
+const clearButton = document.getElementById("clearButton");
+
+clearButton.addEventListener("click", clear);
+
+
+function clear() {
+	document.location.reload();
+	// clear the calculator 
+  }
+  
