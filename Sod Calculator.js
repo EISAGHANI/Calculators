c5e01a8a054c8f3443a2944f2169f0439c20cbda
EISAGHANI <eisabacha.com>
2023-04-10 17:28:30 +0500

    init

--- Sod Calculator.js
+++ Sod Calculator.js
@@ -1,6 +1,5 @@
 const form = document.getElementById("sod-form");
 const totalSqftOutput = document.getElementById("total-sqft");
-// const rollsNeededOutput = document.getElementById("rolls-needed");
 const resultsContainer = document.getElementById("results");
 
 form.addEventListener("submit", (e) => {
@@ -9,21 +8,30 @@ form.addEventListener("submit", (e) => {
 	// Get input values
 	const length = parseFloat(document.getElementById("length").value);
 	const width = parseFloat(document.getElementById("width").value);
-	// const sqftPerRoll = parseFloat(document.getElementById("sqft-per-roll").value);
+	var widthMeasurement = document.getElementById("width-type")
+  // checks to see which measurement is selected and converts it into metres 
+  	if (widthMeasurement = "Centimeters") {
+    	var widthInMetres = width / 100
+ 	 } else if (widthMeasurement = "inches") {
+    	var widthInMetres = width * 39.37
+ 	 } else if (widthMeasurement = "feet") {
+   	 	var widthInMetres = width * 3.28084
+  	} else if (widthMeasurement = "yards") {
+    	var widthInMetres = width * 1.09361
+  	} else if (widthMeasurement = "metres") {
+    	var widthInMetres = width
+  	}
+
 
 	// Calculate total square footage and rolls needed
-	const totalSqft = length * width;
-	// const rollsNeeded = Math.ceil(totalSqft / sqftPerRoll);
+	const totalSqft = length * widthInMetres;
+
 
 	// Output results
 	totalSqftOutput.textContent = totalSqft;
-	// rollsNeededOutput.textContent = rollsNeeded;
 	resultsContainer.style.display = "block";
 });
 const clearButton = document.getElementById("clearButton");
-
 clearButton.addEventListener("click", clear);
-function clear() {
-	// clear the calculator display or reset any other state
-  }
+
   
